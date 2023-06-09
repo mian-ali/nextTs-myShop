2e92ac52e943d2cd393d6273db6b5c536adf23e2
Ali Ahmad <mian41935@gmail.com>
2023-06-10 02:57:37 +0500

    Fake api data fetch in client side #1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,27 @@
+import type {InferGetStaticPropsType} from 'next'
+ import getAllProduct from '../../framework/shopify/product/get-all-product'
 
+export async function getStaticProps() {
 
-export default function Home() {
-  const message: string = 'Hello World'
+  const products= await getAllProduct()
+
+  return{
+
+    props:{
+
+      products
+    },
+    revalidate: 4* 60 * 60
+  }
+}
+export default function Home({
+  products
+}:InferGetStaticPropsType<typeof getStaticProps>) {
+
+  getAllProduct()
   return (
 <div>
-  <h1>{message}</h1>
+  <h1>{JSON.stringify(products)}</h1>
 </div>
   )
 }
